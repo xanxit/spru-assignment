e69dd79ecda7c404486dabce5e7d829cd6a3ba00
Sanchit Hajela <sanchit10hajela@gmail.com>
2023-10-29 19:30:03 +0530

    fix: minor

--- src/sections/Topbar.jsx
+++ src/sections/Topbar.jsx
@@ -6,8 +6,8 @@ export default function Topbar(props) {
         <div className='mx-auto flex flex-row md:max-w-[960px] lg:max-w-[1200px] px-[15px]'>
         <img src={props.data.logo} alt="" className='max-w-[300px] h-[56px] mr-auto'/>
         <div className='pl-[55px] flex flex-col  justify-center'>
-            <h1 className='font-rubik text-[15px] font-[400] text-[#b0b0b0] mb-[3px]'>CALL US ON</h1>
-            <h1 className='font-rubik text-[16px] font-[500] text-black leading-[22px]'>{props.data.phone}</h1>
+            <h1 className='font-rubik text-[15px] font-[400] text-[#b0b0b0] mb-[3px]'>MAIL US ON</h1>
+            <h1 className='font-rubik text-[16px] font-[500] text-black leading-[22px]'>{props.data.email}</h1>
         </div>
         <div className='pl-[55px] flex flex-col justify-center'>
             <h1 className='font-rubik text-[15px] font-[400] text-[#b0b0b0] mb-[3px]'>OFFICE OPENED</h1>
